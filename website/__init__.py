420394801903a3e630aa063839f04bed72ba4cab
RndMnkIII <rndmnkiii@gmail.com>
2021-05-17 17:45:09 +0000

    Setting App routes (Blueprint)

--- website/__init__.py
+++ website/__init__.py
@@ -4,6 +4,13 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'JLZXKJVOASIDFL.KSDFFPOASIDFDFW3E4TR089U'
 
+    from .views import views
+    from .auth import auth
+
+    #Register blueprints with the Flask app
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
     return app
 
 
